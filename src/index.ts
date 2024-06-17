5fdba0c3bee980443ad6a876e5e3c5d8f2dcf532
Henry Gressmann <mail@henrygressmann.de>
2024-06-17 14:22:49 +0200

    chore: i have no idea why i'm updating this project
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
-import { readFileSync } from 'node:fs';
-import http from 'node:http';
-import playSound from 'play-sound';
+import { readFileSync } from "node:fs";
+import http from "node:http";
+import playSound from "play-sound";
 
 const player = playSound();
-const html = readFileSync('./static/index.html', 'utf8');
+const html = readFileSync("./static/index.html", "utf8");
 
 const server = http.createServer((req, res) => {
-	if (req.url === '/') {
-		res.writeHead(200);
+	if (req.url === "/") {
+		res.writeHead(200, { "Content-Type": "text/html" });
 		res.end(html);
 		return;
 	}
 
-	if (req.method !== 'GET' || !req.url?.startsWith('/play/'))
-		return res.end('invalid request');
+	if (req.method !== "GET" || !req.url?.startsWith("/play/"))
+		return res.end("invalid request");
 
-	const audio = req.url.replace('/play/', '');
+	const audio = req.url.replace("/play/", "");
 
-	console.log(`./static/${audio.replace(/[^a-zA-Z]/g, '')}.mp3`);
-	player.play(`./static/${audio.replace(/[^a-zA-Z]/g, '')}.mp3`, {});
+	console.log(`./static/${audio.replace(/[^a-zA-Z]/g, "")}.mp3`);
+	player.play(`./static/${audio.replace(/[^a-zA-Z]/g, "")}.mp3`, {});
 
 	res.writeHead(200);
-	res.end('playing');
+	res.end("playing");
 });
 
-const port = parseInt(process.env.PORT || '3000', 10);
-const host = '0.0.0.0';
+const port = Number.parseInt(process.env.PORT || "3000", 10);
+const host = "0.0.0.0";
 
 server.listen(port, host, () => {
 	console.log(`Server is running on http://${host}:${port}`);
